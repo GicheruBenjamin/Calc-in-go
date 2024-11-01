934fbd05e1f56746c8d4f935edaa87724eee5245
GicheruBenjamin <benjaminnjuguna77@gmail.com>
2024-11-01 03:51:22 +0300

    features/Added modulus feature.

--- main/main.go
+++ main/main.go
@@ -11,6 +11,8 @@ func main() {
 	fmt.Println("2. Subtract")
 	fmt.Println("3. Multiply")
 	fmt.Println("4. Divide")
+	fmt.Println("5. Modulus")
+	fmt.Println("6. Exit")
 
 	var input int
 	fmt.Scanln(&input)
@@ -51,6 +53,17 @@ func main() {
 		} else {
 			fmt.Println("The result is:", divideNumbers(num1, num2))
 		}
+	} else if input == 5 {
+		fmt.Println("Please enter the first number:")
+		var num1 int
+		fmt.Scanln(&num1)
+		fmt.Println("Please enter the second number:")
+		var num2 int
+		fmt.Scanln(&num2)
+		fmt.Println("The result is:", modulusNumbers(num1, num2))
+	} else if input == 6 {
+		fmt.Println("Exiting...")
+		return
 	} else {
 		fmt.Println("Invalid input")
 	}
@@ -70,4 +83,8 @@ func multiplyNumbers(num1, num2 int) int {
 
 func divideNumbers(num1, num2 int) int {
 	return num1 / num2
+}
+
+func modulusNumbers(num1, num2 int) int {
+	return num1 % num2
 }
